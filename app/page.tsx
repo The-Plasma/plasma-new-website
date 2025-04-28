c6cea407c21558413be1195e19e6fefdc4d290d3
Sanket <contact.sanketdhuri@gmail.com>
2025-04-28 18:05:25 +0530

    feat: about page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
 import HomeP from "./main/Home";
 import EmptyScreen from "./main/Empty";
+import ClientShowcase from "./main/About";
 
 export default function Home() {
   return (
     <>
       <HomeP />
+      <ClientShowcase />
       <EmptyScreen />
+
     </>
 
   );
